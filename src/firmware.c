d0f7ee046eb0e336f2f7bba829562b06ee6cbdcc
mauricioduarte01 <mauricioduarte01@gmail.com>
2023-07-07 12:41:56 +0200

    USART implementation

--- src/firmware.c
+++ src/firmware.c
@@ -4,38 +4,47 @@
 #include "core/system.h"
 #include "core/timer.h"
 
-#define LED_PORT      (GPIOA)
-#define LED_PIN       (GPIO5)
+#include "../shared/src/inc/uart.h"
 
-static void gpio_setup(void) {
-  rcc_periph_clock_enable(RCC_GPIOA);
+
+#define LED_PORT (GPIOB)
+#define LED_PIN (GPIO13)
+
+#define UART_PORT (GPIOA)
+#define RX_PIN (GPIO3)
+#define TX_PIN (GPIO2)
+
+static void gpio_setup(void)
+{
+  rcc_periph_clock_enable(RCC_GPIOB);
   // Alternate function 1 (AF1) from GPIOA15 for timer 2 channel 1
-  gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
-  gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+
+  gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, TX_PIN | RX_PIN);
+  gpio_set_af(UART_PORT,GPIO_AF7, TX_PIN | RX_PIN);
 }
 
-int main(void) {
+int main(void)
+{
 
-gpio_setup();
-system_setup();
-timer_setup();
+  system_setup();
+  gpio_setup();
+  uart_setup();
 
-uint64_t start_time = system_get_ticks();
-float duty_cycle = 0.0f;
-
-timer_pwm_set_duty_cycle(duty_cycle);
-
-  while (1) {
-    if(system_get_ticks() - start_time >= 10) {
-    duty_cycle += 1.0f;
-    if (duty_cycle > 100.0f) {
-      duty_cycle = 0.0f;
+  uint64_t start_time = system_get_ticks();
+  while (1)
+  {
+    if (system_get_ticks() - start_time >= 1000)
+    {
+      gpio_toggle(LED_PORT, LED_PIN);
+      start_time = system_get_ticks();
     }
-    timer_pwm_set_duty_cycle(duty_cycle);
 
-    start_time = system_get_ticks();
+    if(uart_data_available()) {
+      uint8_t data = uart_read_byte();
+      uart_write_byte(data + 1);
+    }
+    // do more useful work
   }
-}
-  // Never return
   return 0;
 }
